2a74cb1fee5c0d6051fd48910accad325873d5db
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-02-24 21:19:59 +0530

    circle loaders added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,6 +6,15 @@ const Loader = () => {
   return (
     <div className="loader">
       <img src={loaderlogo} alt="Logo" className="loader-logo" />
+
+      <div className="wrapper">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+      </div>
     </div>
   );
 };
